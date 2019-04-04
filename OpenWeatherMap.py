54bdf358f10e3278827efaf4ff580fd3d0d82e7d
Danil <the.outcast@mail.ru>
2019-04-04 03:10:02 +0300

    commit1

--- OpenWeatherMap.py
+++ OpenWeatherMap.py
@@ -6,6 +6,7 @@ own = pyowm.OWM('792e6a1c6c0e45e707457e35afd35a62')
 observation = own.weather_at_place('Rostov-on-Don,ru')
 weather = observation.get_weather()
 location = observation.get_location()
+translate = {'Rostov-on-Don': 'Ростов-на-Дону'}
 
 print(own)
 print(observation)
@@ -31,3 +32,20 @@ print('Изображение: ' + weather.get_weather_icon_name())
 print('Ветер: ' + str(weather.get_wind()))
 
 
+def WhatIsCloudness():
+    if 0 <= weather.get_clouds() <= 10:
+        return 'ясная'
+
+    if 10 < weather.get_clouds() <= 30:
+        return 'немного облачная'
+
+    if 30 < weather.get_clouds() <= 70:
+        return 'пасмурная'
+
+    if 70 < weather.get_clouds() <= 100:
+        return 'мрачная'
+
+
+print('Погода в городе') + translate[location.get_name()] + ' ' + WhatIsCloudness() + ', температура ' + str(
+    weather.get_temperature('celsius')['temp']) + ' градусов цельсия ' + ', скорость ветра ' + str(
+    weather.get_wind()['speed']) + ' м/с.'
